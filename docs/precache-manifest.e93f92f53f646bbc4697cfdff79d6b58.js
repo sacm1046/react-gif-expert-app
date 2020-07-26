3fbb959f426bcfaba98c226bd72c6faa7ce3cabd
Sebastian Contreras <sacm1046@gmail.com>
2020-07-26 15:51:45 -0400

    change frol build to docs

---
+++ docs/precache-manifest.e93f92f53f646bbc4697cfdff79d6b58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d41659b6079618cef2e7e84a8bad47b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d05bd71e6b1f1f2f484",
+    "url": "/static/css/main.2285cdae.chunk.css"
+  },
+  {
+    "revision": "eb758f2a28b795a30b74",
+    "url": "/static/js/2.2f0f044b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2f0f044b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d05bd71e6b1f1f2f484",
+    "url": "/static/js/main.32061201.chunk.js"
+  },
+  {
+    "revision": "055f3e008571789170d2",
+    "url": "/static/js/runtime-main.3e4d2dea.js"
+  }
+]);
